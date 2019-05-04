fa7106cdf0153d69e3b41c80089619de598798e4
Jacob <jbmenashi@gmail.com>
2019-05-04 11:01:08 -0400

    install packages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+
+const store = createStore(reducer)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
